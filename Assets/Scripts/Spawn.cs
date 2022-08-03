d03201cd1376929395241227a54b277caa8e9845
kolbasist <101858030+kolbasist@users.noreply.github.com>
2022-08-03 23:13:12 +0300

    Update Spawn.cs

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,24 +6,44 @@ public class Spawn : MonoBehaviour
 {
     [SerializeField] private GameObject _template;
     [SerializeField] private uint _spawnperiod = 2;
+    [SerializeField] private Transform  _points;
 
+    private Transform[] _pointsArray;
     private bool _isSpawned = false;
     private uint _spawnCount = 0;
+    private int _pointsCount; 
+    private int _spawnIndex = 0;
 
     private void Start()
     {
-        Debug.Log($"Spawn {_template.name} started every {_spawnperiod} seconds.");
+        _pointsCount = _points.childCount;
+        _pointsArray = new Transform[_pointsCount];
+
+        for (int i = 0; i < _pointsCount; i++)
+        {
+            _pointsArray[i] = _points.GetChild(i);
+        }
+
+        Debug.Log($"Spawn {_template.name} every {_spawnperiod} seconds started.");
     }
 
     private void Update()
     {
         _isSpawned = Time.time / _spawnperiod <_spawnCount;
-        Debug.Log($"{Time.time} - {_isSpawned}");
+        
 
         if (_isSpawned == false)
         {
-            GameObject newObject = Instantiate(_template, new Vector3(0, 0, 0), Quaternion.identity);
+            GameObject newObject = Instantiate(_template, _pointsArray[_spawnIndex].position, Quaternion.identity);
             _isSpawned = true;
+            _spawnCount++;
+            _spawnIndex++;
+            Debug.Log($"{Time.time}, {_spawnCount} spawned");
+        }
+
+        if(_spawnIndex >= _pointsCount)
+        {
+            _spawnIndex = 0;
         }
     }
 }
